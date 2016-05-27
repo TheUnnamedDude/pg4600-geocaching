3baafcc438d3b7784122a3dba767527faca90dea
Tommy Nordli <tommy.nordli@me.com>
2016-05-27 13:54:55 +0200

    Created a list activity

---
+++ app/src/main/java/no/westerdals/pokemon/activities/ListActivity.java
@@ -0,0 +1,15 @@
+package no.westerdals.pokemon.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import no.westerdals.pokemon.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
